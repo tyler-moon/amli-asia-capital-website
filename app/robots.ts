e8a66afacb10eeab7fc91f4376d838f6118d37d5
AMLI <capital.contact@amli.group>
2026-07-09 01:49:50 +0800

    SEO: sitemap in robots (Capital) / Organization JSON-LD schema (Group)

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/"
-    }
+    },
+    sitemap: "https://capital.amli.group/sitemap.xml"
   };
 }
